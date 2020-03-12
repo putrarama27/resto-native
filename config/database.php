838ec9d1e566bf94d2733a683920209dfaaf19f7
Safaatul Laila Putra Ramandan <32947268+putrarama27@users.noreply.github.com>
2020-03-12 12:58:58 +0700

    create menu
    create menu

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php 
+class database{
+	private $db = [
+		'host' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'resto'
+	];
+
+	public function data(){
+		return $this->db;
+	}
+}
